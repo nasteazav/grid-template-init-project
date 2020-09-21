be5bd6320867be470fb5f5cbdf9a33b2cd76090a
Zavalnaia Anastasia <zavalnaia22@gmail.com>
2020-09-21 17:41:17 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# grid-template-init-project
---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').fadeToggle(500);
+  });
+});
